2f0cb9c74fffef88634a098ff9c7ba150e653e26
adrian <adrianjf2002@gmail.com>
2025-12-02 17:38:15 +0100

    day 1

---
+++ day1.rs
@@ -0,0 +1,61 @@
+use std::fs;
+
+fn main() {
+    let input = fs::read_to_string("../data/day1.txt").unwrap();
+
+    let mut pos = 50;
+    let mut hits = 0;
+
+    for line in input.lines() {
+        if line.is_empty() { continue; }
+
+        let dir = line.chars().next().unwrap();
+        let n: i32 = line[1..].parse().unwrap();
+
+        let delta = if dir == 'L' { -n } else { n };
+        pos = (pos + delta).rem_euclid(100);
+
+        if pos == 0 {
+            hits += 1;
+        }
+    }
+
+    println!("{}", hits);
+
+    let mut last_pos = 50;
+    pos = 50;
+    hits = 0;
+
+    for line in input.lines() {
+        if line.is_empty() { continue; }
+
+        let dir = line.chars().next().unwrap();
+        let n: i32 = line[1..].parse().unwrap();
+
+        let delta = if dir == 'L' { -n } else { n };
+        
+        if pos == 0 {
+            hits += (pos + delta).abs()/100;
+        }
+        else {
+            hits += ((pos + delta).abs()/100) + if pos + delta < 0 && pos > 0 {1} else {0};
+        }
+
+        if pos + delta == 0 && (pos + delta).abs()/100 == 0{
+            hits += 1;
+        }
+
+        pos = (pos + delta).rem_euclid(100);
+
+        
+
+        last_pos = pos;
+
+        println!("{}", line);
+        println!("pos: {}", pos);
+        println!("hits: {}", hits);
+        println!();
+    }
+
+    println!("{}", hits);
+}
